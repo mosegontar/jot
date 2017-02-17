cbe9f214e9690938eb6eaf279f787369b611a9cd
Alex Gontar <mosegontar@gmail.com>
2017-02-16 19:56:02 -0500

    Add ENTRY_DIR variable to easily change where entries are stored

--- jot.rb
+++ jot.rb
@@ -1,12 +1,15 @@
 #!/usr/bin/env ruby
 
+ENTRY_DIR = "~/.jot/entries"
+
 def manage_entries(date)
 	todays_entry = date + '.txt'
     launch_editor('vim', todays_entry)
 end
 
 def launch_editor(editor, entry)
-    exec '%s entries/%s' % [editor, entry]
+    exec "#{editor} #{ENTRY_DIR}/#{entry}"
+    # exec '%s %s/%s' % [editor, ENTRY_DIR, entry]
 end
 
 if __FILE__ == $0
